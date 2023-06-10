226ac7a8f003f5be82cd0261dc28d38e226d6c50
Anju1480 <122107513+Anju1480@users.noreply.github.com>
2023-06-10 08:08:29 +0530

    Update report.js

--- public/report.js
+++ public/report.js
@@ -1,3 +1,11 @@
+var AllexportButton = document.getElementById("AllexportButton");
+AllexportButton.addEventListener("click", function () {
+        var workbook=XLSX.utils.table_to_book(document.getElementById("AllTable"), {
+            sheet: "All Users Data"
+        });
+    XLSX.writeFile(workbook, "Allusers_data.xlsx");
+});
+
 var volunteerexportButton = document.getElementById("volunteerexportButton");
 volunteerexportButton.addEventListener("click", function () {
     var workbook = XLSX.utils.table_to_book(document.getElementById("volunteerTable"), { sheet: "volunteer Data" });
