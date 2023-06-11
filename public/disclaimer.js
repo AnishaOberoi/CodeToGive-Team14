53bb986e5373bd2442066ffdaa4a8d62980904cc
Anju1480 <122107513+Anju1480@users.noreply.github.com>
2023-06-11 16:13:29 +0530

    disclaimer css and js files

---
+++ public/disclaimer.js
@@ -0,0 +1,12 @@
+function openPrompt() {
+    document.getElementById('customPrompt').style.display = 'flex';
+  }
+  
+  function closePrompt() {
+    document.getElementById('customPrompt').style.display = 'none';
+  }
+  
+  function goToSurvey() {
+    window.location.href = 'survey.html';
+  }
+  
