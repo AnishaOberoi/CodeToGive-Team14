23c99f246c86d12734d7ade03bec16ebb3e32bf6
KIRTI CHAKARWARTI <kirti1211c@gmail.com>
2023-06-11 16:19:46 +0530

    updated link after prompt of disclaimer

--- public/disclaimer.js
+++ public/disclaimer.js
@@ -1,12 +1,11 @@
 function openPrompt() {
-    document.getElementById('customPrompt').style.display = 'flex';
-  }
-  
-  function closePrompt() {
-    document.getElementById('customPrompt').style.display = 'none';
-  }
-  
-  function goToSurvey() {
-    window.location.href = 'survey.html';
-  }
-  
+  document.getElementById('customPrompt').style.display = 'flex';
+}
+
+function closePrompt() {
+  document.getElementById('customPrompt').style.display = 'none';
+}
+
+function goToSurvey() {
+  window.location.href = '/survey';
+}
